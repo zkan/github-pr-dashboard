f649389b97da2346fdcbf0613ce48f617ed8e54c
zkan <kan@prontomarketing.com>
2017-09-17 09:11:03 +0700

    Create mock-up for project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/semantic-ui/semantic.min.css'
+import './styles/main.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
